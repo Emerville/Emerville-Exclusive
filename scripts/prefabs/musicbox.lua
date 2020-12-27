27ac02a1790900c0bf2a42790e5ef201690aad28
NSavard <digzol@users.noreply.github.com>
2020-12-27 01:48:28 -0500

    Cleanup spaces & tabs

--- scripts/prefabs/musicbox.lua
+++ scripts/prefabs/musicbox.lua
@@ -1,110 +1,101 @@
 local assets =
 {
-	Asset("ANIM", "anim/musicbox.zip"),
-	
-	Asset("ATLAS", "images/inventoryimages/musicbox.xml"),
-	Asset("IMAGE", "images/inventoryimages/musicbox.tex"),	
+    Asset("ANIM", "anim/musicbox.zip"),
+    Asset("ATLAS", "images/inventoryimages/musicbox.xml"),
+    Asset("IMAGE", "images/inventoryimages/musicbox.tex"),
 }
 
 local function turnon(inst)
-    if not inst.components.fueled:IsEmpty() then
-        if inst.components.fueled ~= nil then
-			inst.SoundEmitter:PlaySound("musicbox/sound/playmelody", "christmas")
-            inst.components.fueled:StartConsuming()
-			inst.Light:Enable(true)
-			inst:AddTag("light")
-        end
+    if inst.components.fueled:IsEmpty() then
+        return
     end
+    inst.components.fueled:StartConsuming()
+    inst.Light:Enable(true)
+    inst:AddTag("light")
+    inst.SoundEmitter:PlaySound("musicbox/sound/playmelody", "christmas")
 end
 
 local function turnoff(inst)
-	inst.components.fueled:StopConsuming()
-	inst.Light:Enable(false)
-	inst:RemoveTag("light")
-		
-	inst.SoundEmitter:KillSound("musicbox/sound/playmelody", "christmas")
+    inst.components.fueled:StopConsuming()
+    inst.Light:Enable(false)
+    inst:RemoveTag("light")
+    inst.SoundEmitter:KillSound("musicbox/sound/playmelody", "christmas")
 end
 
 local function OnDropped(inst)
-	turnoff(inst)
-	turnon(inst)
+    turnoff(inst)
+    turnon(inst)
 end
 
 local function MusicBoxCanAcceptFuelItem(self, item)
-if item ~= nil and item.components.fuel ~= nil and (item.components.fuel.fueltype == FUELTYPE.BLUEGEM or item.prefab == "bluegem") then
-		return true
-	else
-		return false
-	end
+    return item ~= nil and item.components.fuel ~= nil
+        and (item.components.fuel.fueltype == FUELTYPE.BLUEGEM or item.prefab == "bluegem")
 end
 
 local function nofuel(inst)
-	SpawnPrefab("sparks").Transform:SetPosition(inst:GetPosition():Get())
-    inst.Light:Enable(false)	
-	inst.SoundEmitter:KillSound("musicbox/sound/playmelody", "christmas")
-	turnoff(inst)
-end 
- 
-local function MusicBoxTakeFuel(self, item) 
-	if self:CanAcceptFuelItem(item) then
-		if item.prefab == "bluegem" then
-			self:DoDelta(480)
-			self.inst.SoundEmitter:PlaySound("dontstarve/common/ancienttable_craft", "sound")
-		end
-			item:Remove()
+    SpawnPrefab("sparks").Transform:SetPosition(inst:GetPosition():Get())
+    inst.SoundEmitter:KillSound("musicbox/sound/playmelody", "christmas")
+    inst.Light:Enable(false)
+    turnoff(inst)
+end
+
+local function MusicBoxTakeFuel(self, item)
+    if self:CanAcceptFuelItem(item) then
+        self:DoDelta(480)
+        self.inst.SoundEmitter:PlaySound("dontstarve/common/ancienttable_craft", "sound")
         return true
     end
-	OnDropped(inst)
+    OnDropped(inst)
 end
 
 local function fn()
-	local inst = CreateEntity()
-    
-	inst.entity:AddTransform()
-	inst.entity:AddAnimState()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
     inst.entity:AddSoundEmitter()
-    inst.entity:AddLight()	
+    inst.entity:AddLight()
     inst.entity:AddNetwork()
-	
+
     MakeInventoryPhysics(inst)
-    
+
     inst.AnimState:SetBank("musicbox")
     inst.AnimState:SetBuild("musicbox")
     inst.AnimState:PlayAnimation("idle", true)
-	
+
     inst.Light:SetFalloff(0.7)
     inst.Light:SetIntensity(.5)
     inst.Light:SetRadius(1)
     inst.Light:SetColour(237/255, 237/255, 209/255)
     inst.Light:Enable(false)
-	
+
     inst:AddTag("musicbox")
-	
+
     inst.entity:SetPristine()
 
     if not TheWorld.ismastersim then
         return inst
-    end	
+    end
 
     inst:AddComponent("inspectable")
-	
-    inst:AddComponent("inventoryitem")	
-	inst.components.inventoryitem.imagename = "musicbox"
-	inst.components.inventoryitem.atlasname = "images/inventoryimages/musicbox.xml"	
-	inst.components.inventoryitem:SetOnDroppedFn(OnDropped)
+
+    inst:AddComponent("inventoryitem")
+    inst.components.inventoryitem.imagename = "musicbox"
+    inst.components.inventoryitem.atlasname = "images/inventoryimages/musicbox.xml"
+    inst.components.inventoryitem:SetOnDroppedFn(OnDropped)
     inst.components.inventoryitem:SetOnPutInInventoryFn(turnoff)
 
-	inst:AddComponent("fueled")
-	inst.components.fueled.accepting = true
-	inst.components.fueled.fueltype = FUELTYPE.BLUEGEM	
-	inst.components.fueled:InitializeFuelLevel(480)
+    inst:AddComponent("fueled")
+    inst.components.fueled.accepting = true
+    inst.components.fueled.fueltype = FUELTYPE.BLUEGEM
+    inst.components.fueled:InitializeFuelLevel(480)
     inst.components.fueled:SetDepletedFn(nofuel)
-	inst.components.fueled.CanAcceptFuelItem = MusicBoxCanAcceptFuelItem
-	inst.components.fueled.TakeFuelItem = MusicBoxTakeFuel	
-	
-    MakeHauntableLaunch(inst)	
-    	
+    inst.components.fueled.CanAcceptFuelItem = MusicBoxCanAcceptFuelItem
+    inst.components.fueled.TakeFuelItem = MusicBoxTakeFuel
+
+    MakeHauntableLaunch(inst)
+
     return inst
 end
 
-return Prefab("common/inventory/musicbox", fn, assets) 
+return Prefab("common/inventory/musicbox", fn, assets)
