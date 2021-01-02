64a1d21a0269321fd69a7e33474b9948bfbf9b3d
NSavard <digzol@users.noreply.github.com>
2021-01-02 09:26:04 -0500

    Cleanup & standardized components

--- scripts/prefabs/musicbox.lua
+++ scripts/prefabs/musicbox.lua
@@ -22,29 +22,41 @@ local function turnoff(inst)
     inst.SoundEmitter:KillSound("musicbox_melody")
 end
 
-local function OnDropped(inst)
+local function ondropped(inst)
     turnoff(inst)
     turnon(inst)
 end
 
-local function MusicBoxCanAcceptFuelItem(self, item)
-    return item ~= nil and item.components.fuel ~= nil
-        and (item.components.fuel.fueltype == FUELTYPE.BLUEGEM or item.prefab == "bluegem")
-end
-
 local function nofuel(inst)
     SpawnPrefab("sparks").Transform:SetPosition(inst:GetPosition():Get())
-    inst.Light:Enable(false)
     turnoff(inst)
 end
 
-local function MusicBoxTakeFuel(self, item)
+local function takefuel(inst)
+    inst.SoundEmitter:PlaySound("dontstarve/common/ancienttable_craft")
+end
+
+local function CanAcceptFuelItem(self, item)
+    return self.accepting and item and item.components.fuel and (item.components.fuel.fueltype == self.fueltype or item.components.fuel.fueltype == self.secondaryfueltype)
+end
+
+local function TakeFuelItem(self, item, doer)
     if self:CanAcceptFuelItem(item) then
-        self:DoDelta(480)
-        self.inst.SoundEmitter:PlaySound("dontstarve/common/ancienttable_craft")
+        local fuelvalue = 480
+        self:DoDelta(fuelvalue, doer)
+
+        if item.components.fuel ~= nil then
+            item.components.fuel:Taken(self.inst)
+        end
+        item:Remove()
+
+        if self.ontakefuelfn ~= nil then
+            self.ontakefuelfn(self.inst)
+        end
+        self.inst:PushEvent("takefuel", { fuelvalue = fuelvalue })
+
         return true
     end
-    OnDropped(inst)
 end
 
 local function fn()
@@ -81,16 +93,17 @@ local function fn()
     inst:AddComponent("inventoryitem")
     inst.components.inventoryitem.imagename = "musicbox"
     inst.components.inventoryitem.atlasname = "images/inventoryimages/musicbox.xml"
-    inst.components.inventoryitem:SetOnDroppedFn(OnDropped)
+    inst.components.inventoryitem:SetOnDroppedFn(ondropped)
     inst.components.inventoryitem:SetOnPutInInventoryFn(turnoff)
 
     inst:AddComponent("fueled")
-    inst.components.fueled.accepting = true
     inst.components.fueled.fueltype = FUELTYPE.BLUEGEM
     inst.components.fueled:InitializeFuelLevel(480)
     inst.components.fueled:SetDepletedFn(nofuel)
-    inst.components.fueled.CanAcceptFuelItem = MusicBoxCanAcceptFuelItem
-    inst.components.fueled.TakeFuelItem = MusicBoxTakeFuel
+    inst.components.fueled:SetTakeFuelFn(takefuel)
+    inst.components.fueled.CanAcceptFuelItem = CanAcceptFuelItem
+    inst.components.fueled.TakeFuelItem = TakeFuelItem
+    inst.components.fueled.accepting = true
 
     MakeHauntableLaunch(inst)
 
