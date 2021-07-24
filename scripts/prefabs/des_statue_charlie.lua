21088c51257e59ac7863d6c6921fc685c724380c
NSavard <digzol@users.noreply.github.com>
2021-07-24 14:51:46 -0400

    Cleanup & renamed timer
    Issue #42

--- scripts/prefabs/des_statue_charlie.lua
+++ scripts/prefabs/des_statue_charlie.lua
@@ -4,11 +4,6 @@ local assets =
 
     Asset("ATLAS", "images/inventoryimages/des_statue_charlie.xml"),
     Asset("IMAGE", "images/inventoryimages/des_statue_charlie.tex"),
-	
-}
-
-local prefabs =
-{
 
 }
 
@@ -16,86 +11,82 @@ local PETALSPERDAY = 5
 local MAXPETALS = 5
 local childtype = "flower"
 
-
 local function PlantFlower(inst)
-	local minrad=1.5
-	local maxrad=2.5
-	
-	--inst exists
-	if not inst then return end
-	local pt = inst:GetPosition()
-	
-	--not too many children
-	local ents = TheSim:FindEntities(pt.x, pt.y, pt.z, maxrad)
-	local count=0
-	for k,v in pairs(ents) do
-		if v.prefab == childtype or v.prefab == "flower_rose" or v.prefab == "flower" then
-			count = count + 1
-		end
-	end
-	if count>=MAXPETALS then return end
-	if math.random()<0.40 then childtype = "flower" end
-	if math.random()<0.40 then childtype = "flower_evil" end
-	if math.random()<0.20 then childtype = "flower_rose" end
-	
-	
-	--Find spot for new child and spawn
-	local angle=math.random()*2*PI
-	local radius=math.random()*(maxrad-minrad)+minrad
-	local offset, check_angle, deflected=FindWalkableOffset(pt, angle, radius, 8, true, false)
-	if(not check_angle) then return end
-	angle=check_angle
-	pt.x=pt.x+radius*math.cos(angle)
-	pt.z=pt.z-radius*math.sin(angle)
-	
-	SpawnPrefab(childtype).Transform:SetPosition(pt:Get())
+    local minrad = 1.5
+    local maxrad = 2.5
+
+    --inst exists
+    if not inst then return end
+    local pt = inst:GetPosition()
+
+    --not too many children
+    local ents = TheSim:FindEntities(pt.x, pt.y, pt.z, maxrad)
+    local count=0
+    for k,v in pairs(ents) do
+        if v.prefab == childtype or v.prefab == "flower_rose" or v.prefab == "flower" then
+            count = count + 1
+        end
+    end
+    if count >= MAXPETALS then return end
+    if math.random() < 0.40 then childtype = "flower" end
+    if math.random() < 0.40 then childtype = "flower_evil" end
+    if math.random() < 0.20 then childtype = "flower_rose" end
+
+
+    --Find spot for new child and spawn
+    local angle = math.random() * 2 * PI
+    local radius = math.random() * (maxrad - minrad) + minrad
+    local offset, check_angle, deflected = FindWalkableOffset(pt, angle, radius, 8, true, false)
+    if(not check_angle) then return end
+    angle = check_angle
+    pt.x = pt.x + radius * math.cos(angle)
+    pt.z = pt.z - radius * math.sin(angle)
+
+    SpawnPrefab(childtype).Transform:SetPosition(pt:Get())
 end
 
 
 local function Animation(inst)
-
-	if inst.currentanim == "work" then
-		if math.random()<0.33 then
-			inst.AnimState:PlayAnimation("fsp_cycle",true)
-			inst.AnimState:SetTime((math.random() * 2)/2)
-		else
-
-			inst.AnimState:PlayAnimation("fsp_idle",true)
-		end
-	else
-
-		inst.AnimState:PlayAnimation("fsp_dry",true)
-	end
+    if inst.currentanim == "work" then
+        if math.random() < 0.33 then
+            inst.AnimState:PlayAnimation("fsp_cycle", true)
+            inst.AnimState:SetTime((math.random() * 2) / 2)
+        else
+            inst.AnimState:PlayAnimation("fsp_idle", true)
+        end
+    else
+        inst.AnimState:PlayAnimation("fsp_dry", true)
+    end
 end
 
 
 local function Dry(inst)
-	inst.currentanim = "fsp_dry"
-	inst.state = "DRY"
-	inst:AddTag("water_sourse_dry")
+    inst.currentanim = "fsp_dry"
+    inst.state = "DRY"
+    inst:AddTag("water_sourse_dry")
 end
 
 local function Fill(inst)
-	inst.currentanim = "work"
-	inst.state = "FULL"
-	inst:RemoveTag("water_sourse_dry")
+    inst.currentanim = "work"
+    inst.state = "FULL"
+    inst:RemoveTag("water_sourse_dry")
 end
 
 local function OnSave(inst, data)
-	data.burnt = inst.state
-	data.daysleft = inst.daysleft
+    data.burnt = inst.state
+    data.daysleft = inst.daysleft
 end
 
 local function OnLoad(inst, data)
-	if data and data.state then
-		inst.state = data.state
-		inst.daysleft = data.daysleft or 2
-		if inst.state == "FULL" then
-			Fill(inst)
-		else
-			Dry(inst)
-		end
-	end
+    if data and data.state then
+        inst.state = data.state
+        inst.daysleft = data.daysleft or 2
+        if inst.state == "FULL" then
+            Fill(inst)
+        else
+            Dry(inst)
+        end
+    end
 end
 
 local function inspect_stat(inst)
@@ -118,68 +109,70 @@ local function fn()
     --inst.entity:AddTag("statue")
 
     MakeObstaclePhysics(inst, 0.75)
-	
-	local minimap = inst.entity:AddMiniMapEntity()
-	minimap:SetIcon( "des_statue_charlie.tex" )
+
+    local minimap = inst.entity:AddMiniMapEntity()
+    minimap:SetIcon( "des_statue_charlie.tex" )
 
     inst.AnimState:SetBank("des_fsp_grand_final_fix")
     inst.AnimState:SetBuild("des_fsp_grand_final_fix")
-	inst.AnimState:PlayAnimation("fsp_cycle",true)
-	
-	inst:AddTag("antlion_sinkhole_blocker")
-	
-	inst.state = "FULL"
-	inst:AddTag("water_sourse")
-	inst:AddTag("water_sourse_nolimit")
-	inst:AddTag("des_statue_charlie")
+    inst.AnimState:PlayAnimation("fsp_cycle",true)
+
+    inst:AddTag("antlion_sinkhole_blocker")
+
+    inst.state = "FULL"
+    inst:AddTag("water_sourse")
+    inst:AddTag("water_sourse_nolimit")
+    inst:AddTag("des_statue_charlie")
     inst:AddTag("structure")
 
-	local scale = 1
-	inst.Transform:SetScale(scale, scale, scale)
-	
+    local scale = 1
+    inst.Transform:SetScale(scale, scale, scale)
+
     inst.entity:SetPristine()
 
     if not TheWorld.ismastersim then
         return inst
     end
 
-	inst:DoPeriodicTask(18, function()
-		Animation(inst)
-	end)
-	
-	inst.daysleft = 5
-	inst.currentanim = "work"
-	---------------Growing details
-	local growth_stages = {
-		{name="only",
-		time=function(inst) return TUNING.TOTAL_DAY_TIME/PETALSPERDAY end,
-		fn=function(inst) PlantFlower(inst) end},
-	}
-	inst:AddComponent("growable")
-	inst.components.growable.stages = growth_stages
-	inst.components.growable:SetStage(1)
-	inst.components.growable.loopstages = true
-	inst.components.growable:StartGrowing()
-	
+    inst:DoPeriodicTask(18, function()
+        Animation(inst)
+    end)
+
+    inst.daysleft = 5
+    inst.currentanim = "work"
+    ---------------Growing details
+    local growth_stages = {
+        {
+            name = "Spawn Flower",
+            time = function(inst) return TUNING.TOTAL_DAY_TIME/PETALSPERDAY end,
+            fn = PlantFlower
+        },
+    }
+    inst:AddComponent("growable")
+    inst.components.growable.stages = growth_stages
+    inst.components.growable:SetStage(1)
+    inst.components.growable.loopstages = true
+    inst.components.growable:StartGrowing()
+
     inst:AddComponent("inspectable")
-	inst.components.inspectable.getstatus = inspect_stat
-	
-		
-	local function OnStartDay(inst)
-		inst.daysleft = inst.daysleft - 1
-		if inst.daysleft < 1 and inst.state == "FULL" then
-			Dry(inst)
-			inst.daysleft = 2 + math.random(2,4)
-		elseif inst.daysleft < 1 and inst.state == "DRY" then
-			Fill(inst)
-			inst.daysleft = 6 + math.random(1,3)
-		end
-	end
+    inst.components.inspectable.getstatus = inspect_stat
+
+
+    local function OnStartDay(inst)
+        inst.daysleft = inst.daysleft - 1
+        if inst.daysleft < 1 and inst.state == "FULL" then
+            Dry(inst)
+            inst.daysleft = 2 + math.random(2,4)
+        elseif inst.daysleft < 1 and inst.state == "DRY" then
+            Fill(inst)
+            inst.daysleft = 6 + math.random(1,3)
+        end
+    end
 
     inst:WatchWorldState("startday", OnStartDay)
-	
-	inst.OnSave = OnSave
-	inst.OnLoad = OnLoad
+
+    inst.OnSave = OnSave
+    inst.OnLoad = OnLoad
 
     return inst
 end
